02c6b3178908174b509ea1979b479c8b7d5aea89
Aanya Jain <aanya31102007@gmail.com>
2026-05-15 19:47:33 +0530

    changed

--- server.js
+++ server.js
@@ -121,7 +121,6 @@ app.use("/api/invoices", invoicesRoutes);
 app.use("/api/payments", paymentsRoutes);
 app.use("/api/inventory", inventoryRoutes);
 app.use("/api/accounting", accountingRoutes);
-app.use("/api/suppliers", suppliersRoutes);
 
 /* -------------------------------------------------------
    404 HANDLER
